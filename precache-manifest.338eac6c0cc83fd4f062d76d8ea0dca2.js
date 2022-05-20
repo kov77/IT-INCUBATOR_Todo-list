5055cf696d3ef3d8e6d9b6dfb680b88c9b2bc24c
Maksym_Koval <maksim6167@gmail.com>
2022-05-20 15:42:11 -0300

    Updates

---
+++ precache-manifest.338eac6c0cc83fd4f062d76d8ea0dca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "936ba842f14921e4b4112b3b9335c947",
+    "url": "/IT-INCUBATOR_Todo-list/index.html"
+  },
+  {
+    "revision": "45d1348ee2decaf1a213",
+    "url": "/IT-INCUBATOR_Todo-list/static/css/main.bcc8012a.chunk.css"
+  },
+  {
+    "revision": "04ae3062e23ced0d66b5",
+    "url": "/IT-INCUBATOR_Todo-list/static/js/2.a8f65d00.chunk.js"
+  },
+  {
+    "revision": "0441e897e96f1c06bed9af9b935029c1",
+    "url": "/IT-INCUBATOR_Todo-list/static/js/2.a8f65d00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d1348ee2decaf1a213",
+    "url": "/IT-INCUBATOR_Todo-list/static/js/main.fcacfb17.chunk.js"
+  },
+  {
+    "revision": "e9d8221b915b8f6493f0",
+    "url": "/IT-INCUBATOR_Todo-list/static/js/runtime-main.27c72303.js"
+  }
+]);
